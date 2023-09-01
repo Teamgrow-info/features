a8c11adcb5efa4bf209d5cfff3e72b62e97bf880
Julien Cartigny <160925+kartoch@users.noreply.github.com>
2023-09-01 22:59:47 +0200

    fix(temporal): manual install instead of cli downloader

--- src/temporal/install.sh
+++ src/temporal/install.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -e
 
-cat > /usr/local/bin/temporal \
-<< EOF
-#!/bin/sh
-curl -sSf https://temporal.download/cli.sh | sh
-EOF
-
-chmod +x /usr/local/bin/temporal
+mkdir /tmp/temporal-install/
+curl -L https://github.com/temporalio/cli/releases/download/v0.10.5/temporal_cli_0.10.5_linux_amd64.tar.gz -o /tmp/temporal-install/temporal.tar.gz
+tar xvzf /tmp/temporal-install/temporal.tar.gz -C /tmp/temporal-install/
+cp /tmp/temporal-install/temporal /usr/local/bin/temporal
