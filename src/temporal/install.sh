f0103cc89347f56ae7302c813111d455d972343b
Julien Cartigny <160925+kartoch@users.noreply.github.com>
2023-09-01 23:14:40 +0200

    fix(temporal): remove temporary install directory

--- src/temporal/install.sh
+++ src/temporal/install.sh
@@ -5,3 +5,4 @@ mkdir /tmp/temporal-install/
 curl -L https://github.com/temporalio/cli/releases/download/v0.10.5/temporal_cli_0.10.5_linux_amd64.tar.gz -o /tmp/temporal-install/temporal.tar.gz
 tar xvzf /tmp/temporal-install/temporal.tar.gz -C /tmp/temporal-install/
 cp /tmp/temporal-install/temporal /usr/local/bin/temporal
+rm -rf /tmp/temporal-install/
